1ac036f00637830c209837b76c38b5748a262fa4
hiroolie <hiroolie@gmail.com>
2017-11-26 19:02:52 +0900

    Edit older versions

--- dbsmgr10/ndb_daylybackup-dbsmgr10.sh
+++ dbsmgr10/ndb_daylybackup-dbsmgr10.sh
@@ -7,54 +7,57 @@ LOGGER='/bin/logger -t NDBBACKUP -i'
 flgfile=/tmp/NDBBACKUP.flg
 
 # バックアップ対象サーバ
-node="ndbd10 ndbd20 ndbd30 ndbd40"
+node="dbdat10 dbdat20 dbdat30 dbdat40"
 
 # 処理開始
 ret=0
 
-echo "Start ndb data backup" | $LOGGER
+echo "Start ndb data backup"
 
 if test -f $flgfile ; then
-	rm -f $flgfile
-	echo "フラグファイルが残っていたため削除しました。" | $LOGGER
+  rm -f $flgfile
+  echo "フラグファイルが残っていたため削除しました。"
 fi
 
-/usr/bin/ndb_mgm -e 'START BACKUP' | $LOGGER
+/usr/bin/ndb_mgm -e 'START BACKUP'
 
 ret=$?
 
 if [ $ret != 0 ] ; then
-	logger -t NDBBACKUP "NDB back up has occured Error"
-	touch $flgfile
+  logger -t NDBBACKUP "NDB back up has occured Error !!"
+  touch $flgfile
 fi
 
 
-echo "Archive dir mount on bkupdir" | $LOGGER
+echo "Archive dir mount on bkupdir"
 
-mount -t nfs bksrv10B:/remote/dbdata /export | $LOGGER
+mount -t nfs bacsrv10:/remote/dbdata /export
 
-if [ -d /export/dbmg10 ] ; then
+if [ -d /export/dbsmgr10 ] ; then
 
-	for ii in ${node} 
-	do
-		echo "ndb data backup from ${ii}" | $LOGGER 
-		/usr/bin/rsync -av --delete root@${ii}:/var/lib/mysql-cluster/BACKUP/ /export/${ii}/ | $LOGGER
-		ret=$?
-		
-		if [ $ret != 0 ] ; then
-			logger -t NDBBACKUP "NDB data archive has occured Error ${ii}"
-			touch $flgfile
-		fi
-		
-	done
+  for ii in ${node}
+  do
+    echo "ndb data backup from ${ii}" 
+    COUNT=1
+    /usr/bin/rsync -av --delete root@${ii}:/ndb_data/mysql-cluster/ndb_data/${COUNT}/BACKUP/ /export/${ii}/
+    ret=$?
+    
+    if [ $ret != 0 ] ; then
+      logger -t NDBBACKUP "NDB data archive has occured Error ${ii} !!"
+      touch $flgfile
+    fi
+    
+    COUNT=$(( COUNT + 1 ))
+    
+  done
 
 else
-	echo "An Error occared in mount backup directory" | $LOGGER
-	ret=1
+  echo "An Error occared in mount backup directory!!"
+  ret=1
 fi
 
-umount /export | $LOGGER
+umount /export
 
-echo "End ndb data backup" | $LOGGER
+echo "End ndb data backup"
 
 exit $ret
--- dbsmgr10/ndb_delete_exceebackup-dbsmgr10.sh
+++ dbsmgr10/ndb_delete_exceebackup-dbsmgr10.sh
@@ -5,30 +5,30 @@ LOGGER='/bin/logger -t NDBBKLOGCLEAR -i'
 
 # 
 node="dbdat10 dbdat20 dbdat30 dbdat40"
-rshcmd=/usr/bin/rsh
+rshcmd=/usr/bin/ssh
 
 # フラグファイル
 flgfile=/tmp/NDBBACKUP.flg
 
 # 実行コマンド
-execcmd='/usr/bin/find /var/lib/mysql-cluster/ndb_data/*/BACKUP -type d -mtime +1 -name "BACKUP-*" | xargs /bin/rm -rfv'
+execcmd='/usr/bin/find /ndb_data/mysql-cluster/ndb_data/*/BACKUP -type d -mtime +1 -name "BACKUP-*" | xargs /bin/rm -rfv'
 
 # 
-echo "Run the delete log files that exceed the retention period." | $LOGGER
+echo "Run the delete log files that exceed the retention period."
 
 if test -f $flgfile ; then
-	rm -f $flgfile
-	echo -e "Previous NDB backup has failed.\nTo cancel the deletion of log files that exceed the retention period." | $LOGGER
-	exit 2
+  rm -f $flgfile
+  echo -e "Previous NDB backup has failed.\nTo cancel the deletion of log files that exceed the retention period."
+  exit 2
 fi
 
 
 for ii in ${node}
 do
-	echo "Delete previous NDB archive log on ${ii}" | ${LOGGER}
-	${rshcmd} -l root ${ii} "${execcmd}" | ${LOGGER}
+  echo "Delete previous NDB archive log on ${ii}"
+  ${rshcmd} root@${ii} "${execcmd}"
 done
 
-echo "To exit the removal process log files that exceed the retention period." | $LOGGER
+echo "To exit the removal process log files that exceed the retention period."
 
 exit 0
