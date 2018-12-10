56ece976e5b5c9875b72e05a5ac6a2928de1713a
hiroolie <hiroolie@gmail.com>
2018-12-11 01:51:52 +0900

    Add mod +x scripts

--- dbsmgr10/ndb_daylybackup-dbsmgr10.sh
+++ dbsmgr10/ndb_daylybackup-dbsmgr10.sh
@@ -3,9 +3,6 @@
 # ログ出力関数(logger)
 LOGGER='/bin/logger -t NDBBACKUP -i'
 
-# フラグファイル
-flgfile=/tmp/NDBBACKUP.flg
-
 # バックアップ対象サーバ
 node="dbdat10 dbdat20 dbdat30 dbdat40"
 
@@ -14,18 +11,13 @@ ret=0
 
 echo "Start ndb data backup"
 
-if test -f $flgfile ; then
-  rm -f $flgfile
-  echo "フラグファイルが残っていたため削除しました。"
-fi
-
-/usr/bin/ndb_mgm -e 'START BACKUP'
+/usr/local/mysql/bin/ndb_mgm -e 'START BACKUP'
 
 ret=$?
 
 if [ $ret != 0 ] ; then
   logger -t NDBBACKUP "NDB back up has occured Error !!"
-  touch $flgfile
+  exit $ret
 fi
 
 
@@ -34,18 +26,12 @@ echo "Archive dir mount on bkupdir"
 mount -t nfs bacsrv10:/remote/dbdata /export
 
 if [ -d /export/dbsmgr10 ] ; then
-
+  COUNT=1
   for ii in ${node}
   do
     echo "ndb data backup from ${ii}" 
-    COUNT=1
-    /usr/bin/rsync -av --delete root@${ii}:/ndb_data/mysql-cluster/ndb_data/${COUNT}/BACKUP/ /export/${ii}/
+    /usr/bin/rsync -av --delete root@${ii}:/var/lib/mysql-cluster/ndb_data/${COUNT}/BACKUP/ /export/${ii}/
     ret=$?
-    
-    if [ $ret != 0 ] ; then
-      logger -t NDBBACKUP "NDB data archive has occured Error ${ii} !!"
-      touch $flgfile
-    fi
     
     COUNT=$(( COUNT + 1 ))
     
--- dbsmgr10/ndb_delete_exceebackup-dbsmgr10.sh
+++ dbsmgr10/ndb_delete_exceebackup-dbsmgr10.sh
@@ -7,21 +7,11 @@ LOGGER='/bin/logger -t NDBBKLOGCLEAR -i'
 node="dbdat10 dbdat20 dbdat30 dbdat40"
 rshcmd=/usr/bin/ssh
 
-# フラグファイル
-flgfile=/tmp/NDBBACKUP.flg
-
 # 実行コマンド
 execcmd='/usr/bin/find /ndb_data/mysql-cluster/ndb_data/*/BACKUP -type d -mtime +1 -name "BACKUP-*" | xargs /bin/rm -rfv'
 
 # 
 echo "Run the delete log files that exceed the retention period."
-
-if test -f $flgfile ; then
-  rm -f $flgfile
-  echo -e "Previous NDB backup has failed.\nTo cancel the deletion of log files that exceed the retention period."
-  exit 2
-fi
-
 
 for ii in ${node}
 do
@@ -31,4 +21,4 @@ done
 
 echo "To exit the removal process log files that exceed the retention period."
 
-exit 0
+exit
